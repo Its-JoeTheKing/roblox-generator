e849e83dd301ccd42226f4e6edaf12892a08aa0e
Its-JoeTheKing <102185866+Its-JoeTheKing@users.noreply.github.com>
2022-11-21 12:08:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const firstStep = () => {
-    document.getElementById("con1").style = "display: none;";
-    document.getElementById("con2").style = "display: block;";
+    var val = document.getElementById("user").value
+    if (val.length === 0) {
+        alert("Enter A Usearname PLease");
+    } else {
+        document.getElementById("con1").style = "display: none;";
+        document.getElementById("con2").style = "display: block;";
+        document.getElementsByTagName("h1")[0].innerText = "Generating Robux To Your Account ("+val+") ...";
+    }
 }
 const secondStep = () => {
     document.getElementById("con2").style = "display: none;";
